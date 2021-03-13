2824fb32bdb7f51df37bb889230004855b04f8a0
ollejj <ollejohansson1996@gmail.com>
2021-03-13 13:19:23 +0100

    added METHOD to log event

--- loghog.js
+++ loghog.js
@@ -10,11 +10,12 @@ let loghog = (options) => {
     this.currentFileData = '';
 
     return async (req, res, next) => {
-        this.event = `${new Date().toLocaleString()} -> ${req.originalUrl} \n`;
+        this.event = `${new Date().toLocaleString()} -> ${req.method}: ${req.originalUrl} \n`;
 
         if (this.options.filePath) {
-            if (existsSync(this.options.filePath))
+            if (existsSync(this.options.filePath)) {
                 this.currentFileData = readFileSync(this.options.filePath);
+            }
             this.currentFileData += this.event;
             writeFileSync(this.options.filePath, this.currentFileData);
             console.log(`Event logged to ${this.options.filePath}`);
